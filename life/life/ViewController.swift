915eba42cf10298b76123df354cf8d30d61d0cbc
Lukas Holmberg <lukazlovicc@hotmail.com>
2020-03-19 10:55:37 +0100

    Init

---
+++ life/life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  life
+//
+//  Created by Lukas Holmberg on 2020-03-19.
+//  Copyright © 2020 Stefan Holmberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
