6fdaa081aac13bed13eba00b7066eefc4d50fcbe
Lukas Holmberg <lukazlovicc@hotmail.com>
2020-04-12 15:53:40 +0200

    todo done

---
+++ life/life/Controller/AddTodoItemVC.swift
@@ -0,0 +1,37 @@
+//
+//  AddTodoItemVC.swift
+//  life
+//
+//  Created by Lukas Holmberg on 2020-04-12.
+//  Copyright © 2020 Stefan Holmberg. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoItemVC : UIViewController {
+    @IBOutlet weak var newTodoTxt: UITextField!
+    
+    
+    @IBAction func Close(_ sender: Any) {
+        self.view.removeFromSuperview()
+    }
+    @IBAction func AddTodoFinal(_ sender: Any) {
+        let parentVC = (self.parent)! as! TodoVC
+        if newTodoTxt.text == "" {
+            let alert = UIAlertController(title: "Alert", message: "You forgot to enter the new item!", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            
+            parentVC.arr.append(newTodoTxt.text ?? "")
+            parentVC.tableView.reloadData()
+            UserDefaults.standard.set(parentVC.arr, forKey: "todoList")
+            self.view.removeFromSuperview()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+    }
+}
--- life/life/Controller/TodoVC.swift
+++ life/life/Controller/TodoVC.swift
@@ -9,12 +9,55 @@
 
 import UIKit
 
-class TodoVC : UIViewController {
+class TodoVC : UIViewController , UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var selectedIndicator: UIView!
+    var arr = UserDefaults.standard.stringArray(forKey: "todoList") ?? [String]()
+    
+    @IBAction func AddTodoItem(_ sender: Any) {
+        let answ = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "raID") as! AddTodoItemVC
+        self.addChild(answ)
+        answ.view.frame = self.view.frame
+        self.view.addSubview(answ.view)
+        answ.didMove(toParent: self)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.selectedIndicator.Animate()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CountryTableViewCell
+        
+        cell.todoTxt.text = arr[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            arr.remove(at: indexPath.row)
+            UserDefaults.standard.set(arr, forKey: "todoList")
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+
+}
+
+class CountryTableViewCell: UITableViewCell {
+    @IBOutlet weak var todoTxt: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
     }
 }
